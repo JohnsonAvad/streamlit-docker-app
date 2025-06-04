434cc5967e96891da5c490ee5bd56459155ae7c8
JohnsonAvad <johnsonavad2023@gmail.com>
2025-06-04 18:11:05 +0300

    Added loan history and CSV download feature

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+try:
+    df = pd.read_csv("loan_record.csv")
+
+except FileNotFoundError:
+    df = pd.DataFrame(columns=["applicant_name","total_amount","months_number","interest_rate","monthly_payment"])    
 
 st.markdown(
     """
@@ -42,6 +49,8 @@ project_title = st.title("Basic Salary Loan Calculator")
 if "result" not in st.session_state:
     st.session_state.result = None
 
+applicant_name = st.text_input("Enter applicant's name")    
+
 
 principal = st.number_input(
     "Enter principal amount", min_value=300000, max_value=10000000
@@ -55,15 +64,12 @@ monthly_interest_rate = st.number_input(
     "Enter monthly interest rate(%)", min_value=0.0, max_value=10.0, value=1.0
 )
 
-total_interest = (monthly_interest_rate / 100) * months_number * principal
 
-total_amount = principal + total_interest
-
-monthly_rate = total_amount / months_number
 
 # Calculate button
 if st.button("Calculate"):
-    total_interest = (monthly_rate / 100) * months_number * principal
+    
+    total_interest = (principal * (monthly_interest_rate / 100)) * months_number 
     total_amount = principal + total_interest
     monthly_payment = total_amount / months_number
 
@@ -79,5 +85,6 @@ if st.session_state.result:
     st.write(f"Total Amount: UGX {st.session_state.result['total_amount']:,.2f}")
     st.write(f"Monthly Payment: UGX {st.session_state.result['monthly_payment']:,.2f}")        
 
-
+st.subheader("Loan History")
+st.dataframe(df)
 
